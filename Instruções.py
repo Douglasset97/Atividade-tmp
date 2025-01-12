6d29d78f5d30dbe399443767c365ea3c630a9f40
douglas silva <douglassilva11@hotmail.com>
2025-01-12 02:07:10 -0300

    foi adicionado instruções para o sistema

---
+++ Instruções.py
@@ -0,0 +1,19 @@
+# Sistema de Recuperação de Informações usando VectorDB e RetrievalQA.
+
+# Descrição do Projeto.
+# Este projeto desenvolve um sistema de recuperação de informações utilizando embeddings vetoriais e um banco de dados vetorial (FAISS) integrado com a biblioteca LangChain.
+
+# Como foi Criado. 
+# A criado é atravez de uma construção de um banco de dados vetorial (FAISS) integrado com a biblioteca LangChain, com objetivo de Desenvolver um sistema que utiliza vetores de embeddings e um banco de dados vetorial para responder perguntas de forma eficiente. Como também foi criado um repositório no GitHub conectado ao sistema do banco de dados.
+
+# Repositório conectado a pasta Atividade-tmp que foi clonada.
+# git clone https://github.com/Douglasset97/Atividade-tmp.git
+
+# A seguir a execursão do sistemas.
+
+# comando para exercutar é esse "python main.py"
+
+# Pergunta: O que é inteligência artificial?
+
+# A Resposta: Geralmente e algum texto que está presente no sistema do banco de dados.
+
